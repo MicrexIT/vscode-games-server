c9d8b2eb14c991ac3d6c502c03fe931e051334ff
Michele Rexha <rexha.michele@gmail.com>
2025-03-21 15:18:23 +0100

    update uri

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.roenx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // MongoDB connection
 mongoose
   .connect(process.env.MONGODB_URI)
